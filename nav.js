1bbe3c7e32bd990788bdab3747ff8f24c724720c
Olgagerassimenko <findirfoodzavod@gmail.com>
2026-07-06 13:18:37 +0500

    add xlsx files and scripts

--- nav.js
+++ nav.js
@@ -8,7 +8,7 @@
     {href:'дашборд_продажи.html',icon:'🛒',label:'Продажи'},
     {href:'дашборд_себестоимость_2025-2026.html',icon:'💹',label:'Себест.'},
     {href:'дашборд_sku_2025-2026.html',icon:'🔍',label:'SKU онлайн'},
-    {href:'дашборд_sku_2025-2026_копия.html',icon:'📤',label:'SKU из Excel'}
+    {href:'дашборд_sku_себестоимость.html',icon:'💲',label:'SKU Себест.'}
   ];
   var cur=location.pathname.split('/').pop()||'index.html';
   var s=document.createElement('style');
