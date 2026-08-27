b3f51c83b51bf7514cff0030c14b621507bfeb25
pulse-ci <ci@local>
2026-08-27 13:22:05 +0000

    Свежие данные дашбордов [skip ci]

--- dds_meta.js
+++ dds_meta.js
@@ -1,1 +1,1 @@
-window.DDS_META={"updated": "26.08.2026"};
+window.DDS_META={"updated": "27.08.2026"};
--- ddsp_meta.js
+++ ddsp_meta.js
@@ -1,1 +1,1 @@
-window.DDSP_META={"updated": "26.08.2026", "through": "25.08.2026"};
+window.DDSP_META={"updated": "27.08.2026", "through": "26.08.2026"};
