458c566d2a92a4b492e71ac8292e415c92fbb1da
pulse-ci <ci@local>
2026-09-04 06:28:46 +0000

    Свежие данные дашбордов [skip ci]

--- dz_kz.js
+++ dz_kz.js
@@ -1,9 +1,5 @@
 window.DZ_KZ = {
-<<<<<<< Updated upstream
-  "updated": "04.09.2026 05:54",
-=======
-  "updated": "04.09.2026 06:10",
->>>>>>> Stashed changes
+  "updated": "04.09.2026 06:18",
   "kz": {
     "total": 200431051,
     "date": "28.08.2026",
